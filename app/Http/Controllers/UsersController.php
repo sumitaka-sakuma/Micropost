2997eca24e463f66a8e7d4801ed9486c251717ca
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-10-02 15:31:20 +0900

    users index

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function index(){
 
-        $users = 'name';
+        $users = User::all();
 
         return view('users.index', compact('users'));
     }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,1 +1,15 @@
-{{ $users }}
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+  <div class="row">
+    <div class="col col-md-4">
+     
+    </div>
+    <div class="column col-md-8">
+        {{ $users }}
+    </div>
+  </div>
+</div>
+@endsection
