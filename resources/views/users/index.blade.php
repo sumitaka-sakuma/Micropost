ac9e7e8339120acfd3c85b5597a522ce935095d4
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-10-02 15:59:03 +0900

    link to users show

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -12,7 +12,7 @@
               @foreach($users as $user)
               <tr>
                 <td>{{ $user->id }}</td>
-                <td>{{ $user->name }}</td>
+                <td><a href="{{ route('users.show', ['id' => $user->id ]) }}">{{ $user->name }}</a></td>
               @endforeach
               </tr>
             </tbody>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,8 +7,20 @@
     <div class="column col-md-8">
       <div class="card">
         <div class="card-header">ユーザー詳細</div>
-          {{$user}}
-          
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">ユーザー名</th>      
+              </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{{ $user->id }}</td>
+              <td>{{ $user->name }}</td>
+            </tr>
+          </tdoby>
+        </table>  
         </div>
       </div>
     </div>
