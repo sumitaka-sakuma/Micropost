a4f71c5a531d5d7b2dbd77f5bcb9004ea5555e9e
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-10-03 12:09:19 +0900

    added contents users show

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -4,11 +4,11 @@
 
 <div class="container">
   <div class="row">
-    <div class="column col-md-8">
+    <div class="column col-md-10">
       <div class="card">
         <div class="card-header">ユーザー詳細</div>
         <br>
-        
+
         <form method="GET" action="{{ route('users.index')}}">
         @csrf
           <div class="form-group">
@@ -22,17 +22,32 @@
           <thead>
             <tr>
               <th scope="col">ID</th>
-              <th scope="col">ユーザー名</th>      
+              <th scope="col">ユーザー名</th>   
+              <th scope="col">生年月日</th>
+              <th scope="col">性別</th>
+              <th scope="col">自己紹介</th>   
               </tr>
           </thead>
           <tbody>
             <tr>
               <td>{{ $user->id }}</td>
               <td>{{ $user->name }}</td>
-              <td><a href="{{ route('users.edit', ['id' => $user->id ]) }}">編集</a></td>
+              <td>{{ $user->birthday }}</td>
+              <td>{{ $user->gender }}</td>
+              <td>{{ $user->self_introduction }}</td>
             </tr>
           </tdoby>
         </table>  
+
+        <form method="GET" action="{{ route('users.edit', ['id' => $user->id ])}}">
+          @csrf
+          <div class="form-group">
+            <div class="text-right">
+              <input class="btn btn-info " type="submit" value="編集"> 
+            </div>
+          </div>
+        </form>
+
         </div>
       </div>
     </div>
