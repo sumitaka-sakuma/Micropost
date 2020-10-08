335dee5c7a96c47f8156f5dc1b67562cacd4d567
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-10-08 16:37:45 +0900

    micropost store

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
-use App\Models\Mocropost;
+use App\Models\Micropost;
 use Illuminate\Support\Facades\Auth;
 
 class MicropostsController extends Controller
@@ -17,8 +17,16 @@ class MicropostsController extends Controller
     }
     
     
-    public function store(){
+    public function store(Request $request){
 
+        $micropost = new Micropost();
+        
+        $micropost->user_id = Auth::id();
+        $micropost->content = $request->input('content');
+
+        $micropost->save();
+
+        return redirect('users/index');
     }
 
     public function destroy($id){
