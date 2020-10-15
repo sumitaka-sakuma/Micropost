d5b855ef6c5e8cdfe098235bbe86095618b43580
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-10-15 16:25:03 +0900

    followers layout

--- resources/views/users/followers.blade.php
+++ resources/views/users/followers.blade.php
@@ -1,4 +1,37 @@
-@foreach($users->followers as $user)
+@extends('layouts.app')
 
-  {{ $user->name }}
-@endforeach
+@section('content')
+
+<div class="container">
+  <div class="row">
+    <div class="column col-md-8">
+      <div class="card">
+        <div class="card-header">フォロー一覧</div>
+          <div class="card-body">
+            
+            <table class="talbe" style="table-layout:fixed;">
+              <tbody>
+                @foreach($users->followers as $user)
+                <tr>
+                  <td><img src="{{ asset('storage/profiles/'.$user->profile_image) }}" style="width:100px; height:100px;"></td>
+                  <td><a href="{{ route('users.show', ['id' => $user->id ]) }}">{{ $user->name }}</a></td>
+                  <td>
+                    @if (auth()->user()->isFollowed($user->id))
+                      <div class="px-2">
+                        <span class="px-1 bg-secondary text-light">フォローされています</span>
+                      </div>
+                    @endif
+                  </td>
+                @endforeach
+                </tr>
+              </tbody>
+            </table>
+
+         
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
