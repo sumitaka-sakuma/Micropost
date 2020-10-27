addc48572cd6ae9828e28f875a6a9674b831dcad
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-10-27 13:58:17 +0900

    test searchForUsers

---
+++ tests/Feature/FormSerchTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use App\Services\FormSearch;
+use App\Models\User;
+use App\Models\Micropost;
+use Tests\TestCase;
+
+class FormSerchTest extends TestCase
+{
+    
+    use RefreshDatabase;
+
+    /*ユーザーの検索機能
+    *  ユーザー二つを作成し、eで検索して二つともヒットすることを確認
+    */
+    public function testSearchForUsers(){
+    
+        DB::table('users')->insert([
+            ['name' => 'testuser', 'email' => 'testuser@gmail.com', 'password' => 'password'],
+            ['name' => 'eric', 'email' => 'eric@gmail.com', 'password' => 'password']
+        ]);
+        
+        $users = DB::table('users')->select('*')->where('name', 'testuser')->orWhere('name', 'eric')->get();
+        
+        $search = 'e';
+        $query = DB::table('users');
+            
+        $query = FormSearch::searchForUsers($search, $query);
+            
+        $result = $query->select('*')->orderBy('created_at', 'desc')->get();
+       
+        $this->assertEquals($users[0], $result[0]);
+        $this->assertEquals($users[1], $result[1]);
+    }
+}
