e641782cf103947e73c7551913be6da053f248df
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-10-30 16:23:38 +0900

    added counter-cache

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -3,9 +3,20 @@
 namespace App\models;
 
 use Illuminate\Database\Eloquent\Model;
+use kanazaca\CounterCache\CounterCache;
 
 class Like extends Model
 {
+
+    use CounterCache;
+
+    public $counterCacheOptions = [
+        'Micropost' => [
+            'field'      => 'likes_count',
+            'foreignKey' => 'micropost_id'
+        ]
+    ];
+
     protected $fillable = [
         'user_id',
         'micropost_id'
